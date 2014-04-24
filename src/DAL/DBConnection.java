eb77b484fd7b43a1abdc912b733f4479ebed2384
Shadowleet <Shadowleet@Shadowleet-Pc>
2014-04-24 13:50:28 +0200

    added DAL

--- src/DAL/DBConnection.java
+++ src/DAL/DBConnection.java
@@ -1,9 +1,46 @@
 package DAL;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
 
 /**
  *
  * @author Shadowleet
  */
+
 public class DBConnection {
+
+    private final Connection m_connection;
+    private static DAL.DBConnection m_instance = null;
     
+    private static final String SERVER_NAME = "localhost";
+    private static final String DATABASE_INSTANCE = "SQLEXPRESS";
+    private static final int PORTNO = 1433; //49197; //58828;
+    private static final String DATABASE_NAME = "Kaj Car Rental";
+    private static final String USERNAME = "Jacob";
+    private static final String PASSWORD = "Jacob123";
+
+    private DBConnection() throws SQLServerException {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName(SERVER_NAME);
+        ds.setInstanceName(DATABASE_INSTANCE);
+        ds.setDatabaseName(DATABASE_NAME);
+        ds.setPortNumber(PORTNO);
+        ds.setUser(USERNAME);
+        ds.setPassword(PASSWORD);
+        m_connection = ds.getConnection();
+    }
+
+    public Connection getConnection() {
+        return m_connection;
+    }
+
+    public static DAL.DBConnection getInstance() throws SQLServerException {
+        if (m_instance == null) {
+            m_instance = new DAL.DBConnection();
+        }
+        return m_instance;
+    }
 }
+
+
