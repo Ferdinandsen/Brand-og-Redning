9238344eb1eb7aa48fdc63c39516e64a14063d3c
Jacob <Jacob@Ferdi>
2014-04-25 10:10:26 +0200

    små ændringer igen

--- src/DAL/DBConnection.java
+++ src/DAL/DBConnection.java
@@ -18,8 +18,8 @@ public class DBConnection {
     private static final String DATABASE_INSTANCE = "SQLEXPRESS";
     private static final int PORTNO = 58828; //1433; //49197;
     private static final String DATABASE_NAME = "Brand og Redning";
-    private static final String USERNAME = "Jakob";
-    private static final String PASSWORD = "Jakob123";
+    private static final String USERNAME = "Jacob";
+    private static final String PASSWORD = "Jacob123";
 //FUCK AF
     private DBConnection() throws SQLServerException {
         SQLServerDataSource ds = new SQLServerDataSource();
