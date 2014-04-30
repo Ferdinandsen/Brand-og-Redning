aab6a3002d82a194fc6965008b8ad59db9227074
Jacob <Jacob@Ferdi>
2014-04-30 16:48:05 +0200

    Hjemmefra Ferdi

---
+++ src/BLL/BLLUsage.java
@@ -0,0 +1,14 @@
+package BLL;
+
+import BE.BEUsage;
+
+/**
+ *
+ * @author Jacob
+ */
+public class BLLUsage {
+
+    public void createReport(BEUsage u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
