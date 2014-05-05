1056a38001a66de82187e491af79f2031768a43e
Shadowleet <Shadowleet@Shadowleet-Pc>
2014-05-05 11:00:04 +0200

    Start mandag + ny forbug

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
 //        CheckInView main = new CheckInView();
-        HLAfterAction1 main = new HLAfterAction1();
+//        HLAfterAction1 main = new HLAfterAction1();
 //        CheckOutView main = new CheckOutView();
 //        HLErrorReport main = new HLErrorReport());
 //        LoginView main = new LoginView();
-//        HLForbrugRapport main = new HLForbrugRapport();
+        HLUsageReport main = new HLUsageReport(null);
         main.setVisible(true);
     }
 }
