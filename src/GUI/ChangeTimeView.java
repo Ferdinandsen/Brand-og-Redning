aaf1d5071172642461b6414a516da440523dceb8
andrethy <AndreThy@10.153.0.143>
2014-05-14 12:34:54 +0200

    hlafteraction1

---
+++ src/GUI/ChangeTimeView.java
@@ -0,0 +1,223 @@
+package GUI;
+
+import BE.BEAppearance;
+import BLL.BLLAppearance;
+import BLL.BLLFireman;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.Timestamp;
+
+public class ChangeTimeView extends javax.swing.JDialog {
+
+    BLLAppearance bllAppearance;
+    BEAppearance localAppearance;
+
+    public ChangeTimeView(BEAppearance appearance) {
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        initComponents();
+        initOtherComponents();
+        localAppearance = appearance;
+        bllAppearance = BLLAppearance.getInstance();
+        fillInformation();
+        calculateHours();
+    }
+
+    private void fillInformation() {
+        txtFornavn.setText(localAppearance.getFireman().getMedarbjeder().getFornavn());
+        txtEfternavn.setText(localAppearance.getFireman().getMedarbjeder().getEfternavn());
+        txtCheckIn.setText(localAppearance.getAlarm().getTimeString());
+        txtCheckUd.setText(localAppearance.getCheckOutString());
+    }
+
+    private void initOtherComponents() {
+        btnOk.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculateHours();
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        txtCheckIn.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    char vChar = e.getKeyChar();
+                    if (!(Character.isDigit(vChar)) || txtCheckIn.getText().length() >=5) {
+                        e.consume();
+                    }
+//                    if (vChar == e.getKe)
+                }
+            });
+        txtCheckIn.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                calculateHours();
+            }
+        });
+
+        txtCheckUd.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                calculateHours();
+            }
+        });
+    }
+
+    private void calculateHours() {
+        String[] checkInHour = txtCheckIn.getText().split(":");
+        String[] checkOutHour = txtCheckUd.getText().split(":");
+        Timestamp checkInHourTs = new Timestamp(0, 0, 0, Integer.parseInt(checkInHour[0]), Integer.parseInt(checkInHour[1]), 33, 66);
+        Timestamp checkOutHourTs = new Timestamp(0, 0, 0, Integer.parseInt(checkOutHour[0]), Integer.parseInt(checkOutHour[1]), 33, 66);
+        long difference = checkOutHourTs.getTime() - checkInHourTs.getTime();
+
+        long second = difference / 1000 % 60;
+        long minute = difference / (1000 * 60) % 60;
+        long hour = difference / (60 * 60 * 1000) % 24;
+        if (second > 0) {
+            minute++;
+        }
+        if (minute > 0) {
+            hour++;
+        }
+        if (hour < 2) {
+            hour = 2;
+        }
+        lblTotal.setText(String.valueOf(hour));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtFornavn = new javax.swing.JTextField();
+        txtEfternavn = new javax.swing.JTextField();
+        txtCheckIn = new javax.swing.JTextField();
+        txtCheckUd = new javax.swing.JTextField();
+        lblTotal = new javax.swing.JLabel();
+        btnOk = new javax.swing.JButton();
+        btnCancel = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setText("Fornavn:");
+
+        jLabel2.setText("Efternavn:");
+
+        jLabel3.setText("Check ind tid:");
+
+        jLabel4.setText("Check ud tid");
+
+        jLabel5.setText("Total:");
+
+        lblTotal.setText("X");
+
+        btnOk.setText("OK");
+
+        btnCancel.setText("Cancel");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtFornavn, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtEfternavn))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtCheckIn))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtCheckUd))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblTotal))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnOk)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnCancel)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtFornavn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtEfternavn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtCheckIn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtCheckUd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(lblTotal))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnOk)
+                    .addComponent(btnCancel))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnOk;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel lblTotal;
+    private javax.swing.JTextField txtCheckIn;
+    private javax.swing.JTextField txtCheckUd;
+    private javax.swing.JTextField txtEfternavn;
+    private javax.swing.JTextField txtFornavn;
+    // End of variables declaration//GEN-END:variables
+}
--- test/BLL/BLLAppearanceTest.java
+++ test/BLL/BLLAppearanceTest.java
@@ -1,59 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package BLL;
-
-import BE.BEAlarm;
-import BE.BEAppearance;
-import java.util.ArrayList;
-import java.util.Date;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-/**
- *
- * @author AndreThy
- */
-public class BLLAppearanceTest {
-    
-    public BLLAppearanceTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    
-    @Test
-    public void testGetAppearancesWithCriteria() {
-        System.out.println("getAppearancesWithCriteria");
-        Date date = null;
-        String time = "10:30";
-        int tolerance = 10;
-        BLLAppearance instance = BLLAppearance.getInstance();
-        ArrayList<BEAppearance> expResult = null;
-        ArrayList<BEAppearance> result = instance.getAppearancesWithCriteria(date, time, tolerance);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//package BLL;
+//
+//import BE.BEAlarm;
+//import BE.BEAppearance;
+//import java.util.ArrayList;
+//import java.util.Date;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+///**
+// *
+// * @author AndreThy
+// */
+//public class BLLAppearanceTest {
+//    
+//    public BLLAppearanceTest() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    
+//    @Test
+//    public void testGetAppearancesWithCriteria() {
+//        System.out.println("getAppearancesWithCriteria");
+//        Date date = null;
+//        String time = "10:30";
+//        int tolerance = 10;
+//        BLLAppearance instance = BLLAppearance.getInstance();
+//        ArrayList<BEAppearance> expResult = null;
+//        ArrayList<BEAppearance> result = instance.getAppearancesWithCriteria(date, time, tolerance);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//}
