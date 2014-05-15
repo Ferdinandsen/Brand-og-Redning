a0c29925fa1fc00ce83c5cdb76dc70f20e885a61
Kaj <Kaj@Andr-PC>
2014-05-15 16:50:08 +0200

    revamp af design

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
 //         CheckInView main = new CheckInView();
-//        MainView main = new MainView();
+        MainView main = new MainView();
 //        HLAfterAction1 main = HLAfterAction1.getInstance();
 //        CheckInView main = new CheckInView();
 //        HLErrorReport main = new HLErrorReport());
-        ILIndsats main = new ILIndsats();
+//        ILIndsats main = new ILIndsats();
         main.setVisible(true);
     }
 }
