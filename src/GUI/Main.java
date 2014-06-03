89dc043dbf6c109eaba5f55ec891e6eb0e5476c3
andrethy <AndreThy@10.153.0.143>
2014-06-03 09:57:07 +0200

    Merge origin/master
    Conflicts:
    	src/BLL/BLLAlarm.java

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -7,14 +7,8 @@ package GUI;
 public class Main {
 
     public static void main(String[] args) {
-//        AddAppearanceView main = new AddAppearanceView();
 //        ViewCheckOut main = new ViewCheckOut();
             ViewMain main = new ViewMain();
-//        HLAfterAction1 main = HLAfterAction1.getInstance();
-//        HLErrorReport main = new HLErrorReport());
-//        ILIndsats main = new ILIndsats();
-//        Administration main = new Administration();
-//        main.setLocationRelativeTo(null);
         main.setVisible(true);
     }
 }
