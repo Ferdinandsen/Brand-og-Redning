d548585cd999503d1a0b179d2984d96261ef2c7a
Shadowleet <Shadowleet@Shadowleet-Pc>
2014-06-03 13:16:08 +0200

    Nu virker alt næsten!

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -7,8 +7,8 @@ package GUI;
 public class Main {
 
     public static void main(String[] args) {
-//        ViewCheckOut main = new ViewCheckOut();
-            ViewMain main = new ViewMain();
+        ViewCheckOut main = new ViewCheckOut();
+//            ViewMain main = new ViewMain();
         main.setVisible(true);
     }
 }
